a9933d7e6de61aaad6086eaa58e4217a8d1bdf27
KazemYavari <yavari.kzm@gmail.com>
2023-09-19 12:10:03 +0330

    develop DeviceCreateView

---
+++ devices/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, re_path
+from .views import DeviceCreateView
+
+
+urlpatterns = [
+    re_path(r"^devices/?$", DeviceCreateView.as_view(), name="create_device"),
+]
--- devices/views.py
+++ devices/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import DeviceSerializer
 
-# Create your views here.
+
+class DeviceCreateView(APIView):
+    serializer_class = DeviceSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        return Response(
+            status=status.HTTP_201_CREATED,
+            data={"message": "Item created successfully."},
+        )
