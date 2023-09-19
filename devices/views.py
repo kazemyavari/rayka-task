c1e3e068f2a40532f2c505ca62a56c1db431f953
KazemYavari <yavari.kzm@gmail.com>
2023-09-19 15:52:20 +0330

    Add handle 500 status

--- devices/views.py
+++ devices/views.py
@@ -9,29 +9,41 @@ class DeviceCreateView(APIView):
     serializer_class = DeviceSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            status=status.HTTP_201_CREATED,
-            data={
-                "message": "Item created successfully.",
-                "data": serializer.validated_data,
-            },
-        )
+        try:
+            serializer = self.serializer_class(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(
+                status=status.HTTP_201_CREATED,
+                data={
+                    "message": "Item created successfully.",
+                    "data": serializer.validated_data,
+                },
+            )
+        except Exception as e:
+            return Response(
+                {"error": "Internal Server Error", "details": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
 
 class DeviceRetrieveView(APIView):
     serializer_class = DeviceSerializer
 
     def get(self, request, pk):
-        device = devices.table.get_item(Key={"id": f"/devices/id{pk}"})
+        try:
+            device = devices.table.get_item(Key={"id": f"/devices/id{pk}"})
 
-        if not "Item" in device:
+            if not "Item" in device:
+                return Response(
+                    status=status.HTTP_404_NOT_FOUND,
+                    data={"message": "This item does not exist."},
+                )
+
+            serializer = self.serializer_class(device["Item"])
+            return Response(status=status.HTTP_200_OK, data={"data": serializer.data})
+        except Exception as e:
             return Response(
-                status=status.HTTP_404_NOT_FOUND,
-                data={"message": "This item does not exist."},
+                {"error": "Internal Server Error", "details": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
-
-        serializer = self.serializer_class(device["Item"])
-        return Response(status=status.HTTP_200_OK, data={"data": serializer.data})
